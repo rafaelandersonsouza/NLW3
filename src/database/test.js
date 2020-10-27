2bcc720219fa9c8e072874122173cad9124e067a
Rafael Anderson de Souza <rafael.anderson42@gmail.com>
2020-10-27 20:37:16 -0300

    orphanges changes

--- src/database/test.js
+++ src/database/test.js
@@ -3,11 +3,14 @@ const saveOrphanage = require('./saveOrphanage');
 
 Database.then(async db => {
 
+    //Ao rodar este arquvo, se o saveOrphanage abaixo não estiver comentado
+    //será adicionado um novo orfanato, cuidado para não repetir dados!
+
     //inserir dados na tabela
     await saveOrphanage(db, {
         lat: "-19.8998467",
-        lng:"-43.9931679",
-        name: "Casa dos meninos",
+        lng:"-43.9831679",
+        name: "Casa das meninas",
         about: "Promove o atendimento de crianças e adolescentes, sob medida de proteção Abrigo em Entidade, buscando auxiliá-los em seu desenvolvimento físico, psicopedagógico e emocional.",
         whatsapp: "994821800",
         images: [
@@ -17,18 +20,23 @@ Database.then(async db => {
         ].toString(),
         instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
         opening_hours: "Horário de visitas: Das 8h até 18h",
-        open_on_weekends: "0"
+        open_on_weekends: "1"
     })
+    
 
     //consultar dados da tabela
    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
    console.log(selectedOrphanages)
 
    //consultar somente 1 orphanato, pelo id
-   const orphanage = await db.all('SELECT * FROM orphanages WHERE id ="2"')
-   console.log(orphanage)
+   //const orphanage = await db.all('SELECT * FROM orphanages WHERE id ="2"')
+   //console.log(orphanage)
 
-   //deletar dado da tabela
-  // console.log(await db.run("DELETE FROM orphanages WHERE id = '4'"))
+   //deletar um dado da tabela
+   //console.log(await db.run("DELETE FROM orphanages WHERE id = '3'"))
   // console.log(await db.run("DELETE FROM orphanages WHERE id = '5'"))
+
+  //Limpar toda tabela e resetar ID (cuidado)
+  //await db.run("DELETE FROM orphanages");
+  //await db.run("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='orphanages';");
 })
